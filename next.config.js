852c47ca9e4decd0485afdf9ac118fa66692ea59
chanmin713 <chanmin713@snu.ac.kr>
2025-12-24 07:32:32 +0900

    fix: enable static export in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export', // 개발 모드에서는 주석 처리 (배포 시에만 사용)
+  output: 'export',
   compress: true,
   poweredByHeader: false,
   generateEtags: false,
 }
 
 module.exports = nextConfig
-
