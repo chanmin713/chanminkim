e5deb5c0e542889e0f2129270dc4245e4a976167
chanmin713 <chanmin713@snu.ac.kr>
2025-12-24 05:04:06 +0900

    style: add header bar container for mobile header

--- components/header/mobile/MobileHeader.tsx
+++ components/header/mobile/MobileHeader.tsx
@@ -5,6 +5,7 @@ import HamburgerButton from './HamburgerButton'
 import MobileMenu from './MobileMenu'
 import MobileLogo from './MobileLogo'
 import MobileCenterName from './MobileCenterName'
+import styles from './MobileHeader.module.css'
 
 export default function MobileHeader() {
   const pathname = usePathname()
@@ -12,11 +13,14 @@ export default function MobileHeader() {
 
   return (
     <>
-      {/* 왼쪽 상단 로고 - 메인 페이지가 아닐 때만 표시 */}
-      {!isHomePage && <MobileLogo />}
+      {/* 모바일 헤더 바 */}
+      <div className={styles.headerBar}>
+        {/* 왼쪽 상단 로고 - 메인 페이지가 아닐 때만 표시 */}
+        {!isHomePage && <MobileLogo />}
 
-      {/* 모바일 햄버거 버튼 */}
-      <HamburgerButton />
+        {/* 모바일 햄버거 버튼 */}
+        <HamburgerButton />
+      </div>
 
       {/* 모바일 메뉴 */}
       <MobileMenu />
